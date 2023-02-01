1d0e0f35d429e6a72a4bde9ca9769f6604466831
7042vs <vinishvini75@gmail.com>
2023-02-01 20:53:17 +0530

    one

---
+++ src/org/employee/EmployeeDetils.java
@@ -0,0 +1,41 @@
+package org.employee;
+
+public class EmployeeDetils { 
+	
+	public EmployeeDetils() {
+		this (123);
+		System.out.println("cts");
+		
+	}
+public EmployeeDetils(int  id) {
+		this("vinish");
+		System.out.println(" 12345");
+		
+	}
+public EmployeeDetils(String Name) {
+	
+	System.out.println("employeeeName : vivnish");
+	
+}
+
+public static void main(String[] args) {
+//	
+	EmployeeDetils e = new EmployeeDetils();
+//	
+//
+//	EmployeeDetils e2= new EmployeeDetils(123456);
+//	EmployeeDetils e3 = new EmployeeDetils("name");
+//	
+//
+//	
+//	
+//	
+	
+}
+
+
+}
+
+
+
+
